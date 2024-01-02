ec62ade004724e4ee5f1ef90665322babba7438b
Christopher Davis <christopherjdavis@gmail.com>
2024-01-02 17:00:17 -0600

    Update nfl_sim_app.py

--- nfl_sim_app.py
+++ nfl_sim_app.py
@@ -371,7 +371,8 @@ if 'pc' in st.session_state:
         st.write(st.session_state['wc'])
     st.altair_chart(st.session_state["superbowl_chart"])
     st.altair_chart(st.session_state["stage_charts"])
-    st.altair_chart(st.session_state["best_chart"])
+    #Week 18, no need for best regular season record
+    #st.altair_chart(st.session_state["best_chart"])
     df_temp = pd.read_csv("data/markets.csv")
     st.write(f'''The following were last updated on {df_temp.loc[0, 'date']}.  Needless to say, do not take the kelly staking sizes literally!!  (Along with errors and imprecisions in our app, also keep in mind how long the stake will be sitting unused.)''')
     st.dataframe(st.session_state['raw_data'])
